087acb0b976835a9e685f450b71f8785573dddce
Bugrova Irina <bugrova147@yandex.ru>
2025-10-07 13:48:14 +0300

    added models User, Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """Модель задачи"""
+
+    # Статусы задачи
+    ACTIVE = "active"
+    COMPLETED = "completed"
+
+    STATUS_CHOICES = [
+        (ACTIVE, "В работе"),
+        (COMPLETED, "Завершена"),
+    ]
+
+    task = models.CharField(
+        max_length=250,
+        verbose_name="Название задачи",
+        help_text="укажите название задачи",
+    )
+    parent_task = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Родительская задача",
+        help_text="Укажите родительскую задачу",
+    )
+    executor = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Исполнитель",
+        help_text="Укажите исполнителя",
+    )
+    time = models.DateTimeField(
+        verbose_name="Когда должно быть готово",
+        help_text="Укажите дату и время, когда должно быть готово",
+    )
+    status = models.CharField(
+        max_length=9,
+        choices=STATUS_CHOICES,
+        default=ACTIVE,
+        verbose_name="Статус задачи",
+        help_text="Укажите статус задачи",
+    )
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+    def __str__(self):
+        return self.task
--- users/models.py
+++ users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None  # Авторизация по email
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта",
+        help_text="Укажите почту",
+    )
+    name = models.CharField(
+        max_length=150,
+        verbose_name="ФИО",
+        help_text="Укажите ФИО"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    position_at_work = models.CharField(
+        max_length=250,
+        verbose_name="Должность",
+        help_text="Укажите должность",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Добавьте аватар",
+    )
+
+    # Авторизация по email
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]
+
+    def __str__(self):
+        return f"{self.name} - {self.position_at_work}"
