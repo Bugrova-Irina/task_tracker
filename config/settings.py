7621b7e608a3250969c5a38d01bc7977759f00bf
Bugrova Irina <bugrova147@yandex.ru>
2025-10-22 16:34:00 +0300

    modified settings.py

--- config/settings.py
+++ config/settings.py
@@ -84,9 +84,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME", "tasks"),
-        "USER": os.getenv("USER", "postgres"),
-        "PASSWORD": os.getenv("PASSWORD", "12345"),
+        "DB_NAME": os.getenv("DB_NAME", "tasks"),
+        "DB_USER": os.getenv("DB_USER", "postgres"),
+        "DB_PASSWORD": os.getenv("DB_PASSWORD", "12345"),
         "HOST": os.getenv("HOST", "db"),
         "PORT": os.getenv("PORT", "5432"),
     }
